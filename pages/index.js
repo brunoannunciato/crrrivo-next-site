e5e1186ed84585a3123526f1ebb3fc761a55b9a6
brunoannunciato <brunobannunciato@gmail.com>
2022-06-26 19:44:17 -0300

    :sparkles: added site title

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Content from '../components/Content';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
@@ -6,6 +7,9 @@ import Services from '../components/Services';
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Crrrivo</title>
+      </Head>
       <Header />
       <Content />
       <Services />
