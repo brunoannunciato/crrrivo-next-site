73ff16be8bd9ff867f9c1536f6a895ee3edba3a8
brunoannunciato <brunobannunciato@gmail.com>
2022-08-08 20:55:20 -0300

    :sparkles: added favicon

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,19 @@ export default function Home() {
     <div>
       <Head>
         <title>Crrrivo</title>
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
       </Head>
       <Header />
       <Content />
