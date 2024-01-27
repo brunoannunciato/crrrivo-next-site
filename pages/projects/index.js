87fc21f20b87083eb13f4d77994466e63fb9a796
brunoannunciato <brunobannunciato@gmail.com>
2024-01-26 21:20:20 -0300

    feat: delete constants

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,10 +2,53 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../../components/Header';
 import Layout from '../../components/Layout';
-import { projects } from './constants';
+import SubHeader from './components/SubHeader';
 
 import "./projects.scss"
-import SubHeader from './components/SubHeader';
+
+const projects = [
+  {
+      id: 1,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+
+  {
+      id: 2,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+
+  {
+      id: 3,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+
+  {
+      id: 4,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+
+  {
+      id: 5,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+
+  {
+      id: 6,
+      image: "/images/placeholder.png",
+      title: "Lorem Ipsum",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore modi tempore sapiente. Quod nulla eos, totam rem explicabo deserunt minus sit harum eaque ducimus expedita voluptates natus dignissimos, a error."
+  },
+]
 
 export default function Projects() {
   return (
