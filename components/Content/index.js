b3a712ff3a18a1194e475916e5623b274cfe1e2e
brunoannunciato <brunobannunciato@gmail.com>
2024-02-13 14:33:57 -0300

    update home text

--- components/Content/index.js
+++ components/Content/index.js
@@ -60,12 +60,7 @@ const Content = () => {
           </div>
           <div className="content__left">
             <h2 className="content__title" ref={leftTitleRef}>
-              <u>
-                Somos um estúdio multidisciplinar de design baseado em São
-                Paulo.{' '}
-              </u>
-              Identificamos oportunidades e desenvolvemos projetos consistentes
-              que ajudam a fortalecer marcas.
+              <u>Somos um estúdio multidisciplinar de design e publicidade baseado em São Paulo.</u> Identificamos oportunidades e desenvolvemos projetos consistentes que ajudam a fortalecer marcas.
             </h2>
           </div>
 
