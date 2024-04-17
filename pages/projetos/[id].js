0d7394107c11946b3e525070a8bdd682bc4e1c19
brunoannunciato <brunobannunciato@gmail.com>
2024-04-16 21:29:11 -0300

    try yo use suspense

--- pages/projetos/[id].js
+++ pages/projetos/[id].js
@@ -7,7 +7,7 @@ import Gioh from '../../Layouts/Gioh'
 import Fyll from '../../Layouts/Fyll'
 import Ana from '../../Layouts/Ana'
 import Cafe from '../../Layouts/Cafe'
-import { useEffect } from "react"
+import { Suspense, useEffect } from "react"
 
 const  themes = {
     leo: Leo,
@@ -31,7 +31,9 @@ const Project = (data) => {
     }, [])
 
     return(
+      <Suspense fallback={<>CARREGANDO</>}>
         <Layout data={data} />
+      </Suspense>
     )
 }
 
