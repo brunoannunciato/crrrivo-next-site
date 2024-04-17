67e4d62ae7e63038a0bf65135c502c773b7ed485
brunoannunciato <brunobannunciato@gmail.com>
2024-04-16 21:19:53 -0300

    feat: add loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
+import { useState } from 'react';
 import { ParallaxProvider } from 'react-scroll-parallax';
 import { AnimatePresence } from 'framer-motion'
+import Router from 'next/router';
 
 import '../styles/index.scss';
 
 function MyApp({ Component, pageProps, router }) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  Router.onRouteChangeStart = () => {
+    console.log("carregando")
+    setIsLoading(true)
+  } 
+
+  Router.onRouteChangeComplete = () => {
+    console.log("CARREGADO")
+    setIsLoading(false)
+  }
+
   return (
+    isLoading ? <>CARREGANDO</> :
     <AnimatePresence mode="wait" initial={false} onExitComplete={() => window.scrollTo(0, 0)}>
       <ParallaxProvider>
         <Component {...pageProps} key={router.asPath}/>
